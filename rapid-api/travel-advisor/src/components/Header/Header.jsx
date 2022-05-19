2669ce462fa70efe6420591e47fc5674a67e4c3d
csernakakos <csernakakos@gmail.com>
2022-05-19 12:35:08 +0200

    Added API key for Maps JavaScript API

--- rapid-api/travel-advisor/src/components/Header/Header.jsx
+++ rapid-api/travel-advisor/src/components/Header/Header.jsx
@@ -1,5 +1,33 @@
+import { Autocomplete } from "@react-google-maps/api";
+import SearchIcon from "@material-ui/icons/Search";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+
+import useStyles from "./styles";
+
 export default function Header() {
+
+    const classes = useStyles();
+
     return (
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    {/* <Autocomplete> */}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}}/>
+                        </div>
+                    {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
