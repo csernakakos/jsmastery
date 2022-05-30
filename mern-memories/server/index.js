2f1d449b6c2e4773a4b3b686d72e3d38d927385d
csernakakos <csernakakos@gmail.com>
2022-05-30 09:09:28 +0200

    Initialized mern-memories

---
+++ mern-memories/server/index.js
@@ -0,0 +1,22 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config({});
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+mongoose
+    .connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server listening on ${PORT}...`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set(`useFindAndModify`, false);
