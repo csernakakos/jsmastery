88e0f2a5893c84005bd8c446033295974310d331
csernakakos <csernakakos@gmail.com>
2022-05-30 14:24:10 +0200

    Created GET_posts, POST_post endpoints

--- mern-memories/server/controllers/postsControllers.js
+++ mern-memories/server/controllers/postsControllers.js
@@ -1,9 +1,29 @@
-import express from "express";
+import PostMessage from "../models/postMessageModel.js";
 
-const GET_posts = (req, res) => {
-    res.send("GET_posts")
+const GET_posts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
 }
 
-export {
+const POST_post = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+    res.send("POST_post")
+}
+
+export { 
     GET_posts,
+    POST_post,
 }
--- mern-memories/server/routes/postsRoutes.js
+++ mern-memories/server/routes/postsRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { GET_posts } from "../controllers/postsControllers.js";
+import { GET_posts, POST_post } from "../controllers/postsControllers.js";
 const router = express.Router();
 
 
 router
     .route("/")
     .get(GET_posts)
+    .post(POST_post)
 
 
 export default router;
