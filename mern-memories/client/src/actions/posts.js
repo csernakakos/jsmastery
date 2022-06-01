41f4341f097bd2fda930bb6df6369e42a10c550e
csernakakos <csernakakos@gmail.com>
2022-06-01 06:39:35 +0200

    1:05

--- mern-memories/client/src/actions/posts.js
+++ mern-memories/client/src/actions/posts.js
@@ -2,8 +2,11 @@ import * as api from "../api";
 
 api.fetchPosts;
 
-const getPosts = () => async (dispatch) => {
-    const action = { type: "FETCH_ALL", payload: []};
-
-    dispatch(action);
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: "FETCH_ALL", payload: data});
+    } catch (error) {
+        console.log(error);
+    }
 }
--- mern-memories/server/index.js
+++ mern-memories/server/index.js
@@ -11,11 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use("/posts", postsRoutes);
-
-app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use("/posts", postsRoutes);
 
 mongoose
     .connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
