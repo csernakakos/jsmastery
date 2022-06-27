70a385e38bcd66dfc79460aea6274aaff38a3695
csernakakos <csernakakos@gmail.com>
2022-06-27 22:38:25 +0200

    Implemented DELETE request, browser-side

--- mern-memories/client/src/components/Posts/Post/Post.jsx
+++ mern-memories/client/src/components/Posts/Post/Post.jsx
@@ -5,9 +5,14 @@ import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import moment from "moment";
+import { useDispatch } from "react-redux";
+import { deletePost } from "../../../actions/posts";
 
 export default function Post({ post, setCurrentId }) {
     const classes = useStyles();
+
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -36,7 +41,7 @@ export default function Post({ post, setCurrentId }) {
                     <ThumbUpAltIcon fontSize="small" />
                     Like {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
